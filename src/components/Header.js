bb8844814583fcb5015bc2ae1aedf7a8da68fcaf
Chris Jones <chrisjonesdesign85@gmail.com>
2023-05-16 19:05:36 -0400

    Added the final files
    Added the final files for the React Task-Tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('click')
-  }
+const Header = ({ title, onAdd, showAdd}) => {
+  
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Hello' onClick={onClick}/>
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
     </header>
   )
 }
 
 Header.defaultProps = {
-  title: 'if/else To-Do'
+  title: 'Task Tracker'
 }
 
 // CSS in JS
